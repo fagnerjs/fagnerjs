7ee11450262ef4a5cf909a2fb5e7a073fb85b1f7
Fagner Janderson da Silva <fjsilva@uolinc.com>
2015-11-03 16:20:40 -0200

    test

--- v2/service-worker.js
+++ v2/service-worker.js
@@ -1,18 +1,12 @@
 'use strict';
 
-self.addEventListener('click', function(event) {
-  console.log('Received a push message', event);
-
-  var title = 'BBBBBYay a message.';
-  var body = 'We have received a push message.';
-  var icon = '/images/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
-  );
+self.addEventListener('push', function(event) {
+    console.log('Received a push message', event);
+    event.waitUntil(
+        self.registration.showNotification('Teste', {
+            body: 'Teste de push',
+            icon: 'http://e.imguol.com/futebol/brasoes/40x40/corinthians.png',
+            tag: 'tag-push'
+        })
+    );
 });
